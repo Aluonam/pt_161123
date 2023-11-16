d9a88d3b95f448ccfb188c15a95b2f5fff627eb9
Paula Cano <paulacanoroman@gmail.com>
2023-11-16 17:14:26 +0100

    feat: funcionalidad botón

--- components/SwitchCapitalLetter.jsx
+++ components/SwitchCapitalLetter.jsx
@@ -3,21 +3,18 @@ import React, {useState} from 'react'
 const SwitchCapitalLetter = () => {
 
     const [inputUser, setInputUser] = useState()
+    const [newSentece, setNewSentece] = useState()
 
     const handleUpperCase = () => {
         const copyArr = structuredClone(inputUser.toUpperCase())
-        
-        // const changeUpperCase = copyArr.map((letter)=>{
-        //     letter.toUpperCase()
-        // })
-        console.log(copyArr)
+        setNewSentece(copyArr)
     }
   return (
     <>
     <input onChange={(e)=>{setInputUser(e.target.value)}} placeholder='Escribe una frase'></input>
     <br/>
     <button onClick={()=>{handleUpperCase()}}>Cambia a mayúsculas</button>
-    {inputUser}
+    {newSentece}
     </>
   )
 }
