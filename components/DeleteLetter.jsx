6dd0203e096e408f7f2a632cc2483baf7e190c5b
Paula Cano <paulacanoroman@gmail.com>
2023-11-16 23:20:38 +0100

    feat: useState y funcionalidad inputs

--- components/DeleteLetter.jsx
+++ components/DeleteLetter.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DeleteLetter = () => {
+
+    const [inputUser, setInputUser] = useState()
+    const [letterDelete, setletterDelete] = useState()
+    
   return (
     <>
     Escribe una frase
-    <input onChange={(e)=>{e.target.value}}></input>
+    <input onChange={(e)=>{setInputUser(e.target.value)}}></input>
+    
     Escribe la letra que quieras eliminar
-    <input onChange={(e)=>{e.target.value}}></input>
+    <input onChange={(e)=>{setletterDelete(e.target.value)}} maxLength="1"></input>
+    {inputUser}{letterDelete}
     <button onClick={()=>{}}>Click para eliminar</button>
     </>
   )
