99960ddc4f7787a7d01e0e93f04d5704392b6f72
Paula Cano <paulacanoroman@gmail.com>
2023-11-16 23:50:56 +0100

    feat: funcionalidad eliminar última letra
    structuredClone
    .split
    .pop
    .join

--- components/DeleteLastLetter.jsx
+++ components/DeleteLastLetter.jsx
@@ -3,11 +3,16 @@ import React, { useState } from 'react'
 const DeleteLastLetter = () => {
 
     const [inputUser, setInputUser] = useState()
-    
+
+    const deleteLast = ()=>{
+        const copyArr = structuredClone(inputUser.split(""))
+        copyArr.pop()
+        setInputUser(copyArr.join(""))
+    }
   return (
     <>
     <input onChange={(e)=>{setInputUser(e.target.value)}}></input>
-    <button onClick={()=>{}}>Borra la última letra</button>
+    <button onClick={()=>{deleteLast()}}>Borra la última letra</button>
     {inputUser}
     </>
   )
