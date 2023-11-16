7ceec6ba0e5e7433caa06663b88fee7b292bc309
Paula Cano <paulacanoroman@gmail.com>
2023-11-16 18:44:14 +0100

    feat: contador funcionalidad

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ModalCounter from './Modalcounter'
 
 const Counter = () => {
+
+    const [number, setNumber] = useState(0)
   return (
     <>
-    <button onClick={()=>{}}>+</button>
-    <button onClick={()=>{}}>-</button>
+    {number}
+    <button onClick={()=>{setNumber(number+1)}}>+</button>
+    <button onClick={()=>{setNumber(number-1)}}>-</button>
     <ModalCounter></ModalCounter>
     </>
   )
