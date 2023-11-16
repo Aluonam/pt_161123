b74037144fe4e273cb62e1b447132dbe8cc9ecf0
Paula Cano <paulacanoroman@gmail.com>
2023-11-16 23:17:19 +0100

    feat: esqueleto del componente
    dos input y un botón

--- components/DeleteLetter.jsx
+++ components/DeleteLetter.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const DeleteLetter = () => {
   return (
-    <div>DeleteLetter</div>
+    <>
+    Escribe una frase
+    <input onChange={(e)=>{e.target.value}}></input>
+    Escribe la letra que quieras eliminar
+    <input onChange={(e)=>{e.target.value}}></input>
+    <button onClick={()=>{}}>Click para eliminar</button>
+    </>
   )
 }
 
